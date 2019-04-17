c992e41e8c785ad2a85f557995f3110add004aa8
Elena Granados <patty518@yahoo.com>
2019-04-16 17:30:52 -0700

    add jsonPlaceholder file and config it to actions and cofir index.js to create store and use redux-thunk

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,16 @@
-export const fetchPosts = () => {
-    return {
-        type: 'FETCH_POSTS'
-    };
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => async dispatch => {
+    const response =  await jsonPlaceholder.get('/posts'); 
+
+    dispatch({ type: 'FETCH_POSTS', payload: response })
 };
+
+// same as:
+export const fetchPostsToo = () => {
+    return async function(dispatch) {
+        const response = await jsonPlaceholder.get('/posts');
+        
+        dispatch({ type: 'FETCH_POSTS', payload: response })
+    };
+};    
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './components/App';
 import reducers from './reducers';
 
+const store = createStore(reducers, applyMiddleware(thunk));
 ReactDOM.render(
     // pass store to Provider, and initialize store to connect all reducers by passing reducers to createStore
-    <Provider store={createStore(reducers)}>
+    <Provider store={store}>
         <App />
     </Provider>,
     document.querySelector('#root'));
