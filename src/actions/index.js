4eeb2313a678de3454afd2aa7d512f00bfed4fc0
Elena Granados <patty518@yahoo.com>
2019-04-18 21:00:43 -0700

    add usersReducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,36 @@
+import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+    // console.log(getState().posts);
+
+    // create an array of unique userIds based on posts
+        // const userIds = _.uniq(_.map(getState().posts, 'userId')); 
+
+    // for each unique userId, we are going to dispatch fetchUser, but we don't have to wait
+    // this time around since we are not displaying the data right away.  We only have to wait
+    // if we are doing somethihgn with the data.  Also, await does not work with forEach.
+        // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+}
+
+// 1 when we call fetchPosts, return a function.  
+// 2 that function will go through thunk
+// 3 thunk will run that function
+// 4 that function will run the async function and wait for response
+// 5 action creator return an 'action' with the fetched data on 'payload' property
+//  6 reducer sees the action, returns the data off the 'payload'
+// 7 if some new state object, redux/react-redux cause app to rerender
 export const fetchPosts = () => async dispatch => {
     const response =  await jsonPlaceholder.get('/posts'); 
 
-    dispatch({ type: 'FETCH_POSTS', payload: response })
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
 };
 
 // same as:
@@ -13,4 +40,20 @@ export const fetchPostsToo = () => {
         
         dispatch({ type: 'FETCH_POSTS', payload: response })
     };
-};    
+}; 
+
+// export const fetchUser = id =>  dispatch => _fetchUser(id, dispatch);
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+//     dispatch({type: 'FETCH_USER', payload: response.data})
+// });
+
+
+export const fetchUser = id =>  async dispatch => {
+    const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({type: 'FETCH_USER', payload: response.data})
+};
+
+
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,11 @@
 import { combineReducers } from 'redux';
+import postsReducer from './postsReducer';
+import usersReducer from './usersReducer';
 
 export default combineReducers({
-    dummyReducer: () => 'hi there'});
+    //name of the property in state is post and it is initialized with 
+    // what is returned by postsReducer, either action.payload if state
+    // changed, or state if unchanged
+    posts: postsReducer,
+    users: usersReducer
+});
